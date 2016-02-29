3934cae474132cb2fee9a53263a4d3ed63daa91d
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2016-02-29 15:26:44 -0500

    Initial Copy of Website Struture
    Created the general website struture, included bootstrap, used php to make a modular website with sections in their own html files.
    
    Used a css, html, js, MISC, resources file strutrue.
    
    Included a README.pdf

--- README.md
+++
@@ -1,1 +0,0 @@
-# iGEMCornellWebsite2016
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+	<meta charset="UTF-8"
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+	<title>Cornell iGEM</title>
+</head>
+<body>
+<!-- Header Section -->
+<?php require("html/header.html"); ?>
+<!-- Navagation Bar -->
+<?php require("html/navbar.html"); ?>
+<!-- Video Section -->
+<?php require("html/video.html"); ?>
+
+<div class="container-fluid">
+	<div class="row">
+		<!-- Various Sections -->
+		<?php require("html/aboutigem.html"); ?>
+		<?php require("html/projects.html"); ?>
+		<?php require("html/achievements.html"); ?>
+		<?php require("html/subteams.html"); ?>
+		<?php require("html/team.html"); ?>
+		<?php require("html/sponsors.html"); ?>		
+	</div>
+</div>
+<!-- Footer Section -->
+<?php require("html/footer.html"); ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html
