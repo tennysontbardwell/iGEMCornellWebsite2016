a3c47f4baf031bc518f902c80b7999054c09a18c
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2016-03-05 02:35:11 -0500

    Small Tweeks/Updates
    Changes from the meeting between Tennyson and Amanda on Monday Feb 29

--- index.php
+++ index.php
@@ -9,29 +9,29 @@
 	<title>Cornell iGEM</title>
 </head>
 <body>
-<!-- Header Section -->
-<?php require("html/header.html"); ?>
-<!-- Navagation Bar -->
-<?php require("html/navbar.html"); ?>
-<!-- Video Section -->
-<?php require("html/video.html"); ?>
+	<!-- Header Section -->
+	<?php require("html/header.html"); ?>
+	<!-- Navagation Bar -->
+	<?php require("html/navbar.html"); ?>
+	<!-- Video Section -->
+	<?php require("html/video.html"); ?>
 
-<div class="container-fluid">
-	<div class="row">
-		<!-- Various Sections -->
-		<?php require("html/aboutigem.html"); ?>
-		<?php require("html/projects.html"); ?>
-		<?php require("html/achievements.html"); ?>
-		<?php require("html/subteams.html"); ?>
-		<?php require("html/team.html"); ?>
-		<?php require("html/sponsors.html"); ?>		
+	<div class="container-fluid">
+		<div class="row">
+			<!-- Various Sections -->
+			<?php require("html/aboutigem.html"); ?>
+			<?php require("html/projects.html"); ?>
+			<?php require("html/achievements.html"); ?>
+			<?php require("html/subteams.html"); ?>
+			<?php require("html/team.html"); ?>
+			<?php require("html/sponsors.html"); ?>		
+		</div>
 	</div>
-</div>
-<!-- Footer Section -->
-<?php require("html/footer.html"); ?>
+	<!-- Footer Section -->
+	<?php require("html/footer.html"); ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script src="js/script.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
 </body>
 </html
