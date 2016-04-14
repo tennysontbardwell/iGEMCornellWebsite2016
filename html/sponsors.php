a05609eecdc57ceb7eb6a63bb45737079692f02b
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2016-04-13 22:35:31 -0400

    Added resoures and small changes

--- html/sponsors.php
+++ html/sponsors.php
@@ -5,4 +5,54 @@
 			<h1>Sponsors</h1>
 		</section>
 	</div>
+	<div class="container">
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_cornellcrest.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_biobasic.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_corning.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_alfaaesar.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_geneious.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_genscript.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_hardydiagnostics.jpg>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_idt.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_mathworks.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_neb.PNG>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_snapgene.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_thermofisher.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_vwr.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/DNA20-logo-for-iGEM.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/KS_Logo_Final.png>
+		</div>
+		<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+			<img class='img-responsive' src=resources/sponsors/version1.0/mendeley-logo-red.png>
+		</div>
+	</div>
 </div>
