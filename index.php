d397bd132f9f2864cd418d11d89c2dc42358e286
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2016-05-07 17:00:26 -0400

    Removing the footer

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 	<?php require("html/sponsors.php"); ?>
 
 	<!-- Footer Section -->
-	<?php require("html/footer.php"); ?>
+	<?php //require("html/footer.php"); ?>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
