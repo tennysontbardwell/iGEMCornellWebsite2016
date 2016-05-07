7886d8126e1578448b080867e79929e950ccc992
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2016-05-07 12:45:41 -0400

    Polishing subteams section, change size and color

--- html/subteams.php
+++ html/subteams.php
@@ -18,24 +18,10 @@
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
+
+
     <div class="item active">
-      <img class="img-responsive center-block" src="resources/subteams/business800x350.jpg" alt="business">
-      <div class='subteam-blerb'>
-        <h2>Business</h2>
-        <p>The business subteam has two primary roles within Cornell iGEM. First, we establish partnerships and sponsorship with many outside organizations to ensure that the project is financially viable. Second, each year we evaluate the marketability of our project and consider applications of our project to improve existing industry. Our coordination with sponsors and development of business models for Cornell iGEM positions us as the fiscal backbone of the team, helping to bring the team’s project ideas to reality.</p>
-      </div>
-    </div>
-
-    <div class="item">
-      <img class="img-responsive center-block" src="resources/subteams/cs800x350.jpg" alt="cs">
-      <div class='subteam-blerb'>
-        <h2>Computer Science/Electrical and Computer Engineering</h2>
-        <p>The CS/ECE Design Team operates at the fusion of synthetic biology and technology.  The team works closely with Product Development in the research and design phases and spearheads all the coding and electrical engineering components of the dry lab product that take our project to the next level.  This includes developing smartphone apps, environmental monitoring systems, and biosensor technology.  In addition, the CS/ECE Design Team creates the competition wiki page and sponsorship video.</p>
-      </div>
-    </div>
-
-    <div class="item">
-      <img class="img-responsive center-block" src="resources/subteams/wetlab800x350.jpg" alt="wetlab">
+      <img class="img-responsive center-block" src="resources/subteams/wetlab1440x470.jpg" alt="wetlab">
       <div class='subteam-blerb'>
         <h2>Wet Lab</h2>
         <p>The wet lab subteam integrates myriad ideas and subjects them to rigorous testing and analysis. Using the most advanced techniques in molecular biology and biochemistry, we design novel experiments to purify our therapeutics, to assess their efficacy, and to test their stability. We are committed to making contributions to synthetic biology through the heterologous expression and the characterization of proteins that remain poorly understood.</p>
@@ -43,7 +29,15 @@
     </div>
 
     <div class="item">
-      <img class="img-responsive center-block" src="resources/subteams/drylab800x350.jpg" alt="drylab">
+      <img class="img-responsive center-block" src="resources/subteams/business1440x470.jpg" alt="business">
+      <div class='subteam-blerb'>
+        <h2>Business</h2>
+        <p>The business subteam has two primary roles within Cornell iGEM. First, we establish partnerships and sponsorship with many outside organizations to ensure that the project is financially viable. Second, each year we evaluate the marketability of our project and consider applications of our project to improve existing industry. Our coordination with sponsors and development of business models for Cornell iGEM positions us as the fiscal backbone of the team, helping to bring the team’s project ideas to reality.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img class="img-responsive center-block" src="resources/subteams/drylab1440x470.jpg" alt="drylab">
       <div class='subteam-blerb'>
         <h2>Product Development</h2>
         <p>The product development subteam is an end-to-end product development team that is user centered and engineering focused, with the end goal of bringing Cornell iGEM's biological engineering technology to the real world. We apply design thinking and various engineering disciplines to explore user needs, generate a problem statement, design and prototype a mechanical solution to the problem, and create product specifications and prepare for manufacture.</p>
@@ -51,12 +45,21 @@
     </div>
 
     <div class="item">
-      <img class="img-responsive center-block" src="resources/subteams/outreach800x350.jpg" alt="outreach">
+      <img class="img-responsive center-block" src="resources/subteams/outreach1440x470.jpg" alt="outreach">
       <div class='subteam-blerb'>
         <h2>Policy and Practices</h2>
         <p>The Policy and Practices subteam contextualizes Cornell iGEM's genetic engineering projects and examines its impact through community engagement and collaborations. The subteam coordinates outreach events such as youth mentorship programs, forums, and expos. Policy and Practices also analyzes policies, and connects those who are familiar and unfamiliar to our project. Through interviews with field experts and interactions with the public, we are reminded that the people we meet always have stories to tell. We aim to share our story with them, too.</p>
       </div>
     </div>
+
+    <div class="item">
+      <img class="img-responsive center-block" src="resources/subteams/cs1440x470.jpg" alt="cs">
+      <div class='subteam-blerb'>
+        <h2>Computer Science/Electrical and Computer Engineering</h2>
+        <p>The CS/ECE Design Team operates at the fusion of synthetic biology and technology.  The team works closely with Product Development in the research and design phases and spearheads all the coding and electrical engineering components of the dry lab product that take our project to the next level.  This includes developing smartphone apps, environmental monitoring systems, and biosensor technology.  In addition, the CS/ECE Design Team creates the competition wiki page and sponsorship video.</p>
+      </div>
+    </div>
+
   </div>
 
   <!-- Left and right controls -->
