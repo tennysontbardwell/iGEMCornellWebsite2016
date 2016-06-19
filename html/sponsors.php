88a80bd87707ce6b469c388105ae6eb48a55ac1a
Tennyson Taylor Bardwell <tennysontaylorbardwell@gmail.com>
2016-06-18 23:40:53 -0400

    Collapsing sponsors

--- html/sponsors.php
+++ html/sponsors.php
@@ -1,60 +1,70 @@
-<section id="sponsors" class="section-header-javascriptTarget">
-	<h1>Sponsors</h1>
-</section>
-<div class="container">
-</div>
-<div class="container">
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_cornellcrest.png>
+<div class="panel-group" id="accordion">
+	<div class="panel panel-default">
+		<!-- <div class="panel-heading"> -->
+			<div id="sponsors" class="section-header-javascriptTarget">
+			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseSponsors">
+					<h1>Sponsors</h1>
+			</a>
+			</div>
+		<!-- </div> -->
+		<div class="container">
+		</div>
+		<div id="collapseSponsors" class="panel-collapse collapse"><div class="panel-body">
+			<div class="container">
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_cornellcrest.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_biobasic.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_corning.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_alfaaesar.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_geneious.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_genscript.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_hardydiagnostics.jpg>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_idt.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_mathworks.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_neb.PNG>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_snapgene.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_thermofisher.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_vwr.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/DNA20-logo-for-iGEM.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/KS_Logo_Final.png>
+				</div>
+				<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
+					<img class='img-responsive' src=resources/sponsors/version1.0/mendeley-logo-red.png>
+				</div>
+			</div>
+		</div></div>
+		<div class="container col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2" style="margin-top: 30px;">
+			<a href="resources/sponsors/Sponsorship-Packet.pdf">
+				<img class='img-responsive' src="resources/buttons/download_sp.png">
+			</a>
+		</div>
 	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_biobasic.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_corning.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_alfaaesar.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_geneious.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_genscript.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_hardydiagnostics.jpg>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_idt.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_mathworks.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_neb.PNG>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_snapgene.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_thermofisher.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/Cornell_vwr.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/DNA20-logo-for-iGEM.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/KS_Logo_Final.png>
-	</div>
-	<div class='col-md-3 col-lg-3 col-sm-3 col-xs-3'>
-		<img class='img-responsive' src=resources/sponsors/version1.0/mendeley-logo-red.png>
-	</div>
-</div>
-<div class="container col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-	<a href="resources/sponsors/Sponsorship-Packet.pdf">
-		<img class='img-responsive' src="resources/buttons/download_sp.png">
-	</a>
 </div>
