2a0c71b2984aad34aca44b28d80af6e458469cad
RiSi95 <rishabh.kr.singh@gmail.com>
2016-07-17 13:31:30 -0400

    Update roster count

--- html/aboutus.php
+++ html/aboutus.php
@@ -5,7 +5,7 @@
 </section>
 <section class="col-md-offset-1 col-md-10 col-lg-offset-2 col-lg-8" style='margin-bottom: 50px;'>
 	<big>
-		<p>The Cornell iGEM Team is comprised of 40 brilliant and creative undergraduate students motivated to save the world through synthetic biology. The team is comprised of five subteams: Wet Lab, Product Development, CS/ECE Design, Policy and Practices, and Business. The team works tirelessly throughout the school year and summer to solve local and global problems related to medical applications, environmental concerns, and human and animal health. We then compete against 280 multidisciplinary teams from all around the world at the iGEM World Jamboree hosted annually by the International Genetically Engineered Machine (iGEM) Foundation.
+		<p>The Cornell iGEM Team is comprised of 32 brilliant and creative undergraduate students motivated to save the world through synthetic biology. The team is comprised of five subteams: Wet Lab, Product Development, CS/ECE Design, Policy and Practices, and Business. The team works tirelessly throughout the school year and summer to solve local and global problems related to medical applications, environmental concerns, and human and animal health. We then compete against 280 multidisciplinary teams from all around the world at the iGEM World Jamboree hosted annually by the International Genetically Engineered Machine (iGEM) Foundation.
 		</p>
 		<p>Check out the Cornell iGEM Team, 2015 winner of Gold, Best Environmental, Best Supporting Entrepreneurship, and Best Applied Design against 280 other international university teams!
 		</p>
